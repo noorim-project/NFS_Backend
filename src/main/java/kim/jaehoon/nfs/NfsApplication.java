f4b98b60ab92b6ff8ecc760865c7b96761a67bfb
by09115 <by09115@outlook.kr>
2019-05-31 20:10:15 +0900

    [CREATE] created SpringBootApplication

---
+++ src/main/java/kim/jaehoon/nfs/NfsApplication.java
@@ -0,0 +1,13 @@
+package kim.jaehoon.nfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfsApplication.class, args);
+    }
+
+}
