eed93693756056a8437378d3f6d4765550930e1b
Jaehoon Kim <by09115@outlook.kr>
2019-06-24 19:37:02 +0900

    [CREATE] 상속받아 사용할 entity 구현 및 임시 유저 domain 생성

--- src/main/java/kim/jaehoon/nfs/user/domain/tempuser/TempUser.java
+++ src/main/java/kim/jaehoon/nfs/user/domain/tempuser/TempUser.java
@@ -1,14 +1,42 @@
 package kim.jaehoon.nfs.user.domain.tempuser;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import kim.jaehoon.nfs.common.domain.BaseTimeEntity;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Getter
 @Entity
 @NoArgsConstructor
 @EqualsAndHashCode
-public class TempUser extends BaseTimeEntity{
+public class TempUser extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 32)
+    private String code;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    public TempUser(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/kim/jaehoon/nfs/user/service/AuthService.java
+++ src/main/java/kim/jaehoon/nfs/user/service/AuthService.java
@@ -1,6 +1,20 @@
 package kim.jaehoon.nfs.user.service;
 
+import kim.jaehoon.nfs.common.response.JwtToken;
+import kim.jaehoon.nfs.user.domain.tempuser.TempUser;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
 public interface AuthService {
 
+    TempUser signup(@Valid @RequestBody SignupDTO dto) throws SendFailedException;
 
+    JwtToken confirmSignup(String code) throws Exception;
+
+    JwtToken signin(SigninDTO dto) throws Exception;
+
+    void sendPasswordResetCode(String email);
+    void confirmPasswordResetCode(String email, String passwordResetCode);
+    void resetPassword(PasswordResetDTO dto);
 }
