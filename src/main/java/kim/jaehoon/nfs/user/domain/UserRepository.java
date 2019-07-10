0c88347c1bc9672faec2bbfb7e7f4eee8b318cee
Jaehoon Kim <by09115@outlook.kr>
2019-07-10 23:39:11 +0900

    [CREATE] 유저 Repository 작성

--- src/main/java/kim/jaehoon/nfs/user/domain/UserRepository.java
+++ src/main/java/kim/jaehoon/nfs/user/domain/UserRepository.java
@@ -1,4 +1,12 @@
 package kim.jaehoon.nfs.user.domain;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
